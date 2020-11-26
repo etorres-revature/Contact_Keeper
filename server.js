0c96d558bb83183e7a81f0d9e2f00c9e79ee2cb7
Eric D. Torres <etorresnotary@gmail.com>
2020-11-26 13:08:36 -0600

    some personal additions to server

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require("express");
-
-const app = express();
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 34825;
 
-app.listen(
-  PORT,
-  () => console.log(`Your app is now listening at http://localhost:${PORT}`)
+const app = express();
+
+app.use(logger("dev"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "welcome to the contact keeper API" });
+});
+
+//Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contact", require("./routes/contact"));
+
+app.listen(PORT, () =>
+  console.log(`Your app is now listening at http://localhost:${PORT}`)
 );
