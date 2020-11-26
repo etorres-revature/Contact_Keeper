aa1e20adfa11d141d0e63f56ef519e62540ce7a7
Eric D. Torres <etorresnotary@gmail.com>
2020-11-26 17:14:35 -0600

    finishing the contact cards on contact item

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -8,14 +8,32 @@ const ContactItem = ({ contact }) => {
       <h3 className="text-primary text-left">
         {name}{" "}
         <span
+          style={{ float: "right" }}
           className={
             "badge " +
             (type === "professional" ? "badge-success" : "badge-primary")
           }
         >
-          {type}
+          {type.charAt("0").toUpperCase() + type.slice(1)}
         </span>
       </h3>
+      <ul className="list">
+        {email && (
+          <li>
+            <i className="fas fa-envelope-open"></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className="fas fa-phone"></i> {phone}
+          </li>
+        )}
+      </ul>
+
+      <p>
+        <button className="btn btn-white btn-sm">Edit</button>
+        <button className="btn btn-danger btn-sm">DELETE</button>
+      </p>
     </div>
   );
 };
