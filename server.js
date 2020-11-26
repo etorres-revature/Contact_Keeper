bc970719d9643c03dc29ff2b65b3df10fa59d21a
Eric D. Torres <etorresnotary@gmail.com>
2020-11-26 14:06:39 -0600

    user model created and validation on the user route

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 34825;
 
 const app = express();
 
+//connect to MongoDB
+connectDB();
+
+//init middleware
 app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
 
 app.get("/", (req, res) => {
   res.json({ msg: "welcome to the contact keeper API" });
