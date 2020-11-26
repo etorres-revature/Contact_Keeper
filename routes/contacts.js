c90985d9447f5a929ede8520d284720bb6dfd6bb
Eric D. Torres <etorresnotary@gmail.com>
2020-11-26 15:02:55 -0600

    new route to create new contact for user

--- routes/contacts.js
+++ routes/contacts.js
@@ -25,21 +25,47 @@ router.get("/", auth, async (req, res) => {
 //@Route        POST api/contacts
 //@desc         Add new contact
 //@access       Private
-router.post("/", (req, res) => {
-  res.send("Add a contact ");
-});
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error!");
+    }
+  }
+);
 
 //@Route        PUT api/contacts/:id
 //@desc         Update one contact
 //@access       Private
-router.put("/:id", (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   res.send("Update contact");
 });
 
 //@Route        DELETE api/contacts/:id
 //@desc         Delete one contact
 //@access       Private
-router.delete("/:id", (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   res.send("Delete contact");
 });
 
