dc38b1a7865bf684ee1476a6e04fa6bc105bb1e3
Eric D. Torres <etorresnotary@gmail.com>
2020-11-26 22:08:23 -0600

    contact add function

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contact", require("./routes/contacts"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 app.listen(PORT, () =>
   console.log(`Your app is now listening at http://localhost:${PORT}`)
