bd201e8fb0a7b3b67506c8999958318493a9e223
Eric D. Torres <etorresnotary@gmail.com>
2020-11-26 18:39:31 -0600

    filter form and funcitonality built

--- client/src/context/contacts/contactReducer.js
+++ client/src/context/contacts/contactReducer.js
@@ -22,6 +22,7 @@ export default (state, action) => {
           contact.id === action.payload.id ? action.payload : contact
         ),
       };
+
     case DELETE_CONTACT:
       return {
         ...state,
@@ -38,6 +39,19 @@ export default (state, action) => {
       return {
         ...state,
         current: null,
+      };
+    case FILTER_CONTACTS:
+      return {
+        ...state,
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, "gi");
+          return contact.name.match(regex) || contact.email.match(regex);
+        }),
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null,
       };
     default:
       return state;
